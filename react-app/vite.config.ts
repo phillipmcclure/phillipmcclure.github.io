9b87f9db4ed640b065731fbd965d9d3be616c7ff
John Dockery <john@summerhealth.com>
2024-12-29 19:14:29 -0500

    fix deployment

--- react-app/vite.config.ts
+++ react-app/vite.config.ts
@@ -6,7 +6,7 @@ export default defineConfig({
   plugins: [react()],
   base: '/experimental/',
   build: {
-    outDir: '../experimental',
+    outDir: 'dist',
     emptyOutDir: true,
   },
   server: {
